6b55c2bd12deffcd3898bfeeb236b2f406d42c7e
HariBalaji24 <sharibalaji2006@gmail.com>
2025-12-19 15:27:01 +0530

    third commit

--- backend/utils/googleconfig.js
+++ backend/utils/googleconfig.js
@@ -3,9 +3,13 @@ import {google} from "googleapis"
 const GOOGLE_CLIENT_ID= process.env.GOOGLE_CLIENT_ID
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
 
-export const oauth2client = new google.auth.OAuth2(
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
-    'postmessage'
-)
+const redirectUri =
+  process.env.NODE_ENV === "production"
+    ? "https://fitnow-zsfx.onrender.com/auth/google/callback"
+    : "http://localhost:3000/auth/google/callback";
 
+export const oauth2client = new google.auth.OAuth2(
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  redirectUri
+);
